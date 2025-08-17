993727438d0ae50524a3a7a08d4f77c3c0305ef0
Ayush singla <143862828+Ayushsingla1@users.noreply.github.com>
2025-08-17 18:55:42 +0530

    frontend initailized

--- dist/generalised.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const addDocuments: (file: any, userId: string) => Promise<{
-    summary: import("@langchain/core/messages").AIMessageChunk;
-    docId: string;
-}>;
-export declare const queryFunc: (query: string, userId: string, conversationId: string) => Promise<{
-    msg: import("@langchain/core/messages").AIMessageChunk;
-}>;
-export declare const createConversation: (userId: string) => Promise<string>;
-//# sourceMappingURL=generalised.d.ts.map
---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='bg-black flex w-screen h-screen justify-center items-center text-white'>
+      pdf summarize + talk
+    </div>
+  )
+}
+
+export default App
